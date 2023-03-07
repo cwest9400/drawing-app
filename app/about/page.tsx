b79a1ab8c85d5311f506f88446dc44f710012e03
cwest9400 <chrstphrwest@gmail.com>
2023-03-07 14:49:07 -0500

    testing link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link"
 
 export default function About() {
   return (
-  <>
+  <main>
   <h4>about page</h4>
-  </>
+  <Link href="/">back to home page</Link>
+  </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <h1>hello world</h1>
+      <Link href="/about">About page</Link>
     </main>
   )
 }
